e33969060b2b4595a83f5d7b916c2815369465eb
Olivier Pierre <olivierpierre85@gmail.com>
2024-04-28 11:57:20 +0200

    install empty template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>Etymo {{ value }}</div>
 </template>
 
 <script>
@@ -7,7 +7,7 @@ export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      value: "*"
     };
   }
 };
